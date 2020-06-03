1d6d7dc9bcab053decbeb5ed94ee9f7ab4da594a
Soulmates2 <kaist984@kaist.ac.kr>
2020-06-04 01:20:21 +0900

    Initial Merge

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import {Button, Card, CardDeck} from 'react-bootstrap';
 
 const MainStyle = styled.div`
-  padding-top: 5rem;
-  padding-left: 5rem;
-  padding-right: 5rem;
+  padding-top: 4rem;
+  padding-left: 0.5rem;
   position: absolute;
   z-index: 1;
   width: 100%;
@@ -14,38 +12,9 @@ const MainStyle = styled.div`
 `;
 
 const Main = props => {
-  return <MainStyle>
-  <h3>Hello! This is Roomtopia. Offer or Discover your Short-Term Rental House Quickly.</h3><br/><br/><br/>
-  <div style={{width: '100%', margin: 'auto', paddingBottom: 10}}>
-   <b style={{fontSize: '25px'}}>CURRENT OFFERS</b> <Button style={{float: 'right'}} href="RegisterRoomPage" variant="primary">+ Register Offer</Button>
-  </div>
-  <CardDeck style={{width: '50%'}}>
-	<Card style={{ width: '18rem' }}>
-	  <Card.Img variant="top" src={require('../images/Sample.png')} />
-	  <Card.Body>
-	    <Card.Title>Card Title</Card.Title>
-	    <Card.Text>
-	      Some quick example text to build on the card title and make up the bulk of
-	      the card's content.
-	    </Card.Text>
-	    <Button variant="primary">Go somewhere</Button>
-	  </Card.Body>
-	</Card>{' '}
-	<Card style={{ width: '18rem' }}>
-	  <Card.Img variant="top" src={require('../icons/menu.png')} />
-	  <Card.Body>
-	    <Card.Title>Card Title</Card.Title>
-	    <Card.Text>
-	      Some quick example text to build on the card title and make up the bulk of
-	      the card's content.
-	    </Card.Text>
-	    <Button variant="primary">Go somewhere</Button>
-	  </Card.Body>
-	</Card>
-	<Card/>
-  </CardDeck>
+  const { children } = props;
 
-  </MainStyle>
+  return <MainStyle>{children}</MainStyle>;
 };
 
 export default Main;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import Header from '../components/Header';
+import {Button, Card, CardDeck} from 'react-bootstrap';
 import Main from '../components/Main';
 
 const MainPage = props => {
   return (
     <>
-        <Header/>
-
-        <Main/>
+      <Header/>
+      <Main>
+        <h3>Hello! This is Roomtopia. Offer or Discover your Short-Term Rental House Quickly.</h3><br/><br/><br/>
+        <div style={{width: '100%', margin: 'auto', paddingBottom: 10}}>
+        <b style={{fontSize: '25px'}}>CURRENT OFFERS</b> <Button style={{float: 'right'}} href="RegisterRoom" variant="primary">+ Register Offer</Button>
+        </div>
+        <CardDeck style={{width: '50%'}}>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={require('../images/Sample.png')} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>{' '}
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={require('../icons/menu.png')} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the bulk of
+              the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+        <Card/>
+        </CardDeck>
+      </Main>
     </>
   );
 };
