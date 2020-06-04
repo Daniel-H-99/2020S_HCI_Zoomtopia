fb10ff317fa7bb22019a5be95010d49105a6b543
Taeyoon <cloudloverty@kaist.ac.kr>
2020-06-04 21:07:19 +0900

    add contents to MyPage

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Main from '../components/Main';
 import firebase from '../components/Firestore';
+import {Card, CardDeck, Badge, ListGroup, ListGroupItem} from 'react-bootstrap';
 
 const MyPage = props => {
   const db = firebase.firestore();
@@ -14,7 +15,26 @@ const MyPage = props => {
       <Header/>
       <Main>
         <div>This is MyPage</div>
-        <div>Testing place for firestore</div>
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={require('../images/Sample.png')} />
+            <Card.Body>
+                <Card.Title>Room Name</Card.Title>
+                <Card.Text>
+                Description Text Here
+                </Card.Text>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+                <ListGroupItem>Period</ListGroupItem>
+                <ListGroupItem>Cost</ListGroupItem>
+                <ListGroupItem>Contract Status</ListGroupItem>
+            </ListGroup>
+            <Card.Body>
+                <Card.Link href="#">View detail</Card.Link>
+                <Card.Link href="#">
+                    Check Request<Badge variant="light">4</Badge>
+                </Card.Link>
+            </Card.Body>
+            </Card>
       </Main>
     </>
   );
