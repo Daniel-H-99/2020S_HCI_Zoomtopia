8019d7b8ee9026bf5206117aa14e084665ca3ba0
Taeyoon <cloudloverty@kaist.ac.kr>
2020-06-08 17:01:55 +0900

    edit mypageCard, redesign iteminfo, connect userID with DB

--- src/pages/ItemInfo.jsx
+++ src/pages/ItemInfo.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import Header from '../components/Header';
-import Main from '../components/Main';
 import ItemInfoForm from '../components/ItemInfoForm';
+import {Button} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const ItemInfo = props => {
   return (
     <>
-        <ItemInfoForm userID="user4"/>
+        <ItemInfoForm userID={props.user}/>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <Link to="/MyPage">
+              <Button>Go to My Page</Button>  
+          </Link>
+        </div>
     </>
   );
 };
